5a535d8560b8308805df140ad84d2c55b1ce0da3
jfeo <jens@feodor.dk>
2017-06-30 10:33:57 +0200

    Add ArchiveVersion class.

---
+++ LibHardHorn/ArchiveVersion.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HardHorn.ArchiveVersion
+{
+    class ArchiveVersion
+    {
+        List<Table> _tables = new List<Table>();
+        public IEnumerable<Table> Tables { get { return _tables; } }
+
+        string _id;
+        public string ID { get { return _id; } }
+
+        string _path;
+        public string Path { get { return _path; } }
+
+        public ArchiveVersion(string id, string path, IEnumerable<Table> tables)
+        {
+            _tables = tables.ToList<Table>();
+            _id = id;
+            _path = path;
+        }
+
+        public 
+    }
+}
