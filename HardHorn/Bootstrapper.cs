15858f9478d592a01c00c44c50b35fbfccb81fcc
jfeo <jens@feodor.dk>
2017-08-14 15:53:50 +0200

    StringFormat on progress counter changed. CurrentCulture set.

--- HardHorn/Bootstrapper.cs
+++ HardHorn/Bootstrapper.cs
@@ -1,5 +1,7 @@
 ﻿using System.Windows;
 using Caliburn.Micro;
+using System.Windows.Markup;
+using System.Globalization;
 
 namespace HardHorn
 {
@@ -12,6 +14,8 @@ namespace HardHorn
 
         protected override void OnStartup(object sender, StartupEventArgs e)
         {
+
+            FrameworkElement.LanguageProperty.OverrideMetadata(typeof(FrameworkElement), new FrameworkPropertyMetadata(XmlLanguage.GetLanguage(CultureInfo.CurrentCulture.IetfLanguageTag)));
             DisplayRootViewFor<ViewModels.MainViewModel>();
         }
     }
