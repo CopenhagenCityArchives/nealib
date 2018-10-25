9cb6f6b8d00877f2545edc944e1c66980c7e6459
jfeo <jens@feodor.dk>
2018-10-25 14:26:08 +0200

    Capture all unicode whitespace characters with Blank test.

--- LibHardHorn/Analysis/Test.cs
+++ LibHardHorn/Analysis/Test.cs
@@ -280,11 +280,46 @@ namespace HardHorn.Analysis
 
         public class Blank : Test
         {
+            static char[] whitespaces = new char[]
+            {
+                '\u0009',
+                '\u000A',
+                '\u000B',
+                '\u000C',
+                '\u000D',
+                '\u0020',
+                '\u0085',
+                '\u00A0',
+                '\u1680',
+                '\u2000',
+                '\u2001',
+                '\u2002',
+                '\u2003',
+                '\u2004',
+                '\u2005',
+                '\u2006',
+                '\u2007',
+                '\u2008',
+                '\u2009',
+                '\u200A',
+                '\u2028',
+                '\u2029',
+                '\u202F',
+                '\u205F',
+                '\u3000',
+                '\u180E',
+                '\u200B',
+                '\u200C',
+                '\u200D',
+                '\u2060',
+                '\uFEFF'
+            };
             public override AnalysisTestType Type { get { return AnalysisTestType.BLANK; } }
 
             bool IsWhitespace(char c)
             {
-                return c == ' ' || c == '\n' || c == '\t' || c == '\r';
+                return whitespaces.Any(w => c == w);
+                //return c == ' ' || c == '\n' || c == '\t' || c == '\r';
             }
 
             public override Result GetResult(Post post, Column column)
