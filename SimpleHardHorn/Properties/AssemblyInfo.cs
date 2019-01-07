58c170f9be4a745ff2196a4aaef9dff055063798
jfeo <jens@feodor.dk>
2019-01-07 18:54:00 +0100

    Edit name, new icon.

--- SimpleHardHorn/Properties/AssemblyInfo.cs
+++ SimpleHardHorn/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SimpleHardHorn")]
+[assembly: AssemblyTitle("HardHorn Light")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SimpleHardHorn")]
+[assembly: AssemblyProduct("HardHorn Light")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
