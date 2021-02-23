826b864bda7f700d91c979013303a15226311c34
Bo Henriksen <stumpdenmark@hotmail.com>
2021-02-23 11:56:44 +0100

    Update FileIndexReader.cs
    Simplify xml reading of FileIndex.xml. Use linq instead of XML stream.

--- nealib/src/Utility/FileIndexReader.cs
+++ nealib/src/Utility/FileIndexReader.cs
@@ -2,112 +2,42 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Xml;
 using System.Xml.Linq;
 
 namespace NEA.Utility
 {
 
-    public class FileIndexReader : IDisposable
+    public class FileIndexReader
     {
-        Stream _stream;
-        XmlReader _xmlReader;
-        XNamespace xmlnsxsi = "http://www.w3.org/2001/XMLSchema-instance";
         ArchiveVersion _archiveversion;
+        FileInfo FilePath;
 
         public FileIndexReader(ArchiveVersion archiveversion)
         {
+            FilePath = new FileInfo(Path.Combine(archiveversion.Path, archiveversion.Id + ".1", "Indices", "fileIndex.xml"));
+            
             _archiveversion = archiveversion;
-            _stream = new FileStream(Path.Combine(archiveversion.Path, "Indices", "fileIndex.xml"), FileMode.Open, FileAccess.Read);
-            _xmlReader = XmlReader.Create(_stream);
+
+            if (!FilePath.Exists)
+            {
+                throw new Exception(String.Format("Could not find fileIndex.xml in this path", FilePath));
+            }
         }
 
         /// <summary>
-        /// Read rows of the table.
+        /// Read files from fileIndex.xml
         /// </summary>
-        /// <param name="rows"></param>
-        /// <param name="n"></param>
-        /// <returns></returns>
-        public int ReadN(out AVFile[] files, int n = 100000, int offset = 0)
+        /// <returns>IEnumerable<AVFile></returns>
+        public IEnumerable<AVFile> ReadFiles()
         {
-            files = new Archiving.AVFile[n];
-            int row = 0;
+            XDocument fileIndex = XDocument.Load(FilePath.FullName);
+            var ns = fileIndex.Root.Name.Namespace;
 
-            while (row < n && _xmlReader.Read())
-            {
-                if (_xmlReader.NodeType == XmlNodeType.Element && _xmlReader.Name.Equals("f"))
-                {
-                    using (XmlReader inner = _xmlReader.ReadSubtree())
-                    {
-                        if (inner.Read())
-                        {
-                            files[row] = new AVFile(
-                                XElement.Load(inner).Element(xmlnsxsi + "foN").Value, 
-                                XElement.Load(inner).Element(xmlnsxsi + "fiN").Value,
-                                _archiveversion);
-                        }
-                    }
-                    row++;
-                }
-            }
-
-            return row;
+            return from f in fileIndex.Descendants(ns.GetName("f"))
+                   select new AVFile(f.Element(ns.GetName("foN")).Value, f.Element(ns.GetName("fiN")).Value, _archiveversion, f.Element(ns.GetName("md5")).Value);
         }
-
-        public IEnumerable<AVFile> Read(int chunkSize = 100000, int offset = 0)
-        {
-            int rowsRead = 0;
-            AVFile[] files;
-
-            while ((rowsRead = ReadN(out files, chunkSize, offset)) > 0)
-            {
-                for (int i = 0; i < rowsRead; i++)
-                {
-                    yield return files[i];
-                }
-            }
-
-            yield break;
-        }
-
-        #region IDisposable Support
-        private bool disposedValue = false; // To detect redundant calls
-
-        protected virtual void Dispose(bool disposing)
-        {
-            if (!disposedValue)
-            {
-                if (disposing)
-                {
-                    _xmlReader.Close();
-                    _xmlReader.Dispose();
-                    _stream.Close();
-                    _stream.Dispose();
-                }
-
-                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
-                // TODO: set large fields to null.
-
-                disposedValue = true;
-            }
-        }
-
-        // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
-        ~FileIndexReader()
-        {
-            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
-            Dispose(false);
-        }
-
-        // This code added to correctly implement the disposable pattern.
-        public void Dispose()
-        {
-            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
-            Dispose(true);
-            // TODO: uncomment the following line if the finalizer is overridden above.
-            GC.SuppressFinalize(this);
-        }
-        #endregion
     }
 
 }
